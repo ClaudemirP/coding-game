d6d9d5b45249524a1e531ca158ee8587e8b048a0
Claudemir Ribeiro da Silva <claudemirp@outlook.com>
2022-01-18 11:37:11 -0300

    Create temperatures.js

---
+++ temperatures.js
@@ -0,0 +1,23 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const n = parseInt(readline()); // the number of temperatures to analyse
+var inputs = readline().split(' ');
+for (let i = 0; i < n; i++) {
+    const t = parseInt(inputs[i]);// a temperature expressed as an integer ranging from -273 to 5526
+}
+
+// Write an answer using console.log()
+if( n == 0){
+    console.log(0)
+} else {
+    let res = inputs.sort((a,b) => a - b)
+    if(res[res.length - 1] < 0){
+        console.log(res[res.length - 1])
+    } else {
+        console.log(res.find(item => item >= 0))
+    }
+}
+// To debug: console.error('Debug messages...');
